ee182958675d13408fd89b7572dacb8b7d437856
Ayush Baid <ayushrakeshbaid@gmail.com>
2013-11-10 19:30:19 +0530

    Fare revised

---
+++ src/com/exzalt/meterdown/WebViewActivity.java
@@ -0,0 +1,63 @@
+package com.exzalt.meterdown;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.webkit.WebView;
+
+import com.actionbarsherlock.app.SherlockActivity;
+
+
+public class WebViewActivity extends SherlockActivity{
+	private WebView webView;
+	 
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.webview);
+		
+		Intent intent=getIntent();
+		int cityno=intent.getIntExtra("cityno",0);
+		int choice=intent.getIntExtra("choice",0);
+		
+		String data="";
+		switch (choice)
+		{
+			case 0:
+				switch (cityno)
+				{
+					case 0:
+						data="<body>" + "<img src=\"mumbaiAuto.png\"/></body>";
+						break;
+					case 1:
+						data="<body>" + "<img src=\"puneAuto.png\"/></body>";
+						break;
+					case 2:
+						data="<body>" + "<img src=\"bengaluruAuto.png\"/></body>";
+						break;
+					case 4:
+						data="<body>" + "<img src=\"hyderabadAuto.png\"/></body>";
+						break;
+					case 5:
+						data="<body>" + "<img src=\"delhiAuto.png\"/></body>";
+						break;
+				}
+				break;
+			case 1:
+				switch(cityno)
+				{
+					case 0:
+						data="<body>" + "<img src=\"mumbaiTaxi.png\"/></body>";
+						break;
+					case 3:
+						data="<body>" + "<img src=\"kolkataTaxi.png\"/></body>";
+						break;
+					case 5:
+						data="<body>" + "<img src=\"delhiTaxi.png\"/></body>";
+						break;
+				}
+		
+		}
+ 
+		webView = (WebView) findViewById(R.id.webview);
+		webView.loadDataWithBaseURL("file:///android_asset/",data , "text/html", "utf-8",null);
+	}
+};
